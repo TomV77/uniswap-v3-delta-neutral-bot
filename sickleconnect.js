38d84edf177e41d7b9c7f87247b06bc00cf93988
TomV77 <lowportmusic@gmail.com>
2025-12-16 11:52:04 +0100

    Change Web3 provider to Ankr RPC endpoint

--- sickleconnect.js
+++ sickleconnect.js
@@ -1,13 +1,2 @@
 const Web3 = require('web3');
-const web3 = new Web3(new Web3.providers.HttpProvider('https://base-mainnet.infura.io/v3/c0660434a7f448b0a99f1b5d049e95e6'));
-
-async function testConnection() {
-    try {
-        const blockNumber = await web3.eth.getBlockNumber();
-        console.log('Current Block Number:', blockNumber);
-    } catch (error) {
-        console.error('Error connecting to Web3:', error);
-    }
-}
-
-testConnection();
+const web3 = new Web3('https://rpc.ankr.com/base'); // Alternative RPC endpoint
